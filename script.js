67f8181e6bb5cfbe68a439d12361d8e45b7a99a3
bitcoinchile <bitcoinchile2008@gmail.com>
2024-10-05 14:54:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ async function fetchBitcoinPrice() {
     const response = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
     const data = await response.json();
     const price = parseFloat(data.price);
-    document.getElementById('bitcoinPrice').innerText = `Price: $${price.toFixed(2)}`;
+    document.getElementById('bitcoinPrice').innerText = `Price: $${price.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+`;
     return price;
 }
 
